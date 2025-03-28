d40ae5bf12f025dce8a44563347bcb8b3ae2f44a
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-03-28 17:36:11 -0400

    Fixed a spelling mistake

--- themes/myfirstcustomtheme/archive-product.php
+++ themes/myfirstcustomtheme/archive-product.php
@@ -1,6 +1,6 @@
 <?php
 get_header();
-$shopFeaturedImage = wp_get_attachement_image_src( get_post_thumbnail_id( wc_get_page_id( 'shop' ) ), 'full' );
+$shopFeaturedImage = wp_get_attachment_image_src( get_post_thumbnail_id( wc_get_page_id( 'shop' ) ), 'full' );
 ?>
 <section class="shop-masthead" style="background: url('<?php echo $shopFeaturedImage[0]; ?>');">
     <div>
