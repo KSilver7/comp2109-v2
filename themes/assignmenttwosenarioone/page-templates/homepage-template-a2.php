5762ba821f46446ff0d553efdaae6bdb4e8912d9
Kristina Hocken <200213022@student.georgianc.on.ca>
2025-03-29 00:29:34 -0400

    fixed a big typo in my link to my css

--- themes/assignmenttwosenarioone/page-templates/homepage-template-a2.php
+++ themes/assignmenttwosenarioone/page-templates/homepage-template-a2.php
@@ -29,7 +29,7 @@ get_header();
         </div>
     </section>
     <section class="custom-plugin-section row">
-        <?php echo do_shortcode("[silverbooksshop]"); ?>
+        <!-- <?php echo do_shortcode("[silverbooksshop]"); ?> -->
     </section>
 </main>
 <?php
